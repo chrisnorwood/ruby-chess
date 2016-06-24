f50808219353aa32fe9ef8defad56549aa056f21
Chris Norwood <cnorwood92@gmail.com>
2016-06-24 07:36:32 -0700

    Board data structure, 8x8 matrix array: rows of column-items

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,8 @@
 class Board
+  attr_accessor :data
+
+  def initialize
+    @data = Array.new(8, Array.new(8, nil))
+  end
+
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe Board do
+  before :all do
+    @board = Board.new
+  end
+
+  context "#initialize" do
+    it "initializes @data as array of 8 arrays (rows)" do
+      expect(@board.data.size).to eql 8
+    end
+
+    it "initializes @data submembers to have 8 column values" do
+      expect(@board.data.all? {|i| i.size == 8} ).to eql true
+    end
+  end
 
 end
