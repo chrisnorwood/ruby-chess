8d7b20c3e29a2e556d38dd559bd088642ca03c5a
Chris Norwood <cnorwood92@gmail.com>
2016-07-03 15:04:28 -0700

    Player has color attribute

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,5 +1,15 @@
 require 'player'
 
 describe Player do
+  before :all do
+    @player1 = Player.new(:w)
+    @player2 = Player.new(:b)
+  end
 
+  context "#initialize" do
+    it "has color attribute" do
+      expect(@player1).to have_attributes(:color => :w)
+      expect(@player2).to have_attributes(:color => :b)
+    end
+  end
 end
