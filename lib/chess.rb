0988fe6bad5d25b2753d562df30a46d7d675f649
Chris Norwood <cnorwood92@gmail.com>
2016-07-18 21:50:53 -0700

    Add basic initialize structure to Chess

--- lib/chess.rb
+++ lib/chess.rb
@@ -1,2 +1,15 @@
+require_relative 'board'
+
+# pieces
+require_relative 'pieces/bishop'
+require_relative 'pieces/king'
+require_relative 'pieces/knight'
+require_relative 'pieces/pawn'
+require_relative 'pieces/queen'
+require_relative 'pieces/rook'
+
 class Chess
+  def initialize(board)
+    
+  end
 end
