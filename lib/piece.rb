b20afa9004f4ca414df88dff9ccd0c5ec5df8a0c
Chris Norwood <cnorwood92@gmail.com>
2016-07-18 22:49:42 -0700

    Create possible_moves public method relies on child class @move_vectors

--- lib/piece.rb
+++ lib/piece.rb
@@ -1,8 +1,26 @@
 class Piece
   attr_accessor :color, :position, :icon
+  attr_reader   :move_vectors
 
   def initialize(color, position)
-    @color = color
+    @color    = color
     @position = position
+  end
+
+  public
+
+  def possible_moves
+    moves = []
+
+    move_vectors.each do |vector|
+      shift_x, shift_y  = vector[0], vector[1]
+      start_x, start_y  = position[0], position[1]
+
+      moves << [start_x+shift_x, start_y+shift_y]
+    end
+
+    moves.select! { |move| move[0] >= 0 && move[0] <= 8 && move[1] >= 0 && move[1] <= 8 }
+    
+    return moves
   end
 end
