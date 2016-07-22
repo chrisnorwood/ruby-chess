7561c7174f25fe236a27495a5335b4f0fb828ab3
Chris Norwood <cnorwood92@gmail.com>
2016-07-21 22:22:00 -0700

    Finish tested vector creation for Pawn

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -4,5 +4,31 @@ class Pawn < Piece
   def initialize(color, position)
     super(color, position)
     @icon = 'P'
+    create_vectors
+  end
+
+  private
+
+  def create_vectors
+    case color
+    when :w
+      white_vectors
+    when :b
+      black_vectors
+    end
+  end
+
+  def white_vectors
+    move_vectors << [0,1] << [1,1] << [-1,1]
+    if y_position == 1
+      move_vectors << [0,2]
+    end
+  end
+
+  def black_vectors
+    move_vectors << [0,-1] << [1,-1] << [-1,-1]
+    if y_position == 6
+      move_vectors << [0,-2]
+    end
   end
 end
--- spec/pieces/pawn_spec.rb
+++ spec/pieces/pawn_spec.rb
@@ -1,11 +1,52 @@
 require 'pieces/pawn'
 
 describe Pawn do
-  before :all do
-    @pawn = Pawn.new(:w, [0,1])
+  subject(:white_pawn) { Pawn.new(:w, [0,1]) }
+
+  subject(:black_pawn) { Pawn.new(:b, [0,6]) }
+
+
+  describe "#initialize" do
+    it { expect(black_pawn).to be_kind_of Piece }
   end
 
-  context "#initialize" do
-    it { expect(@pawn).to be_kind_of Piece }
+  describe "#possible_moves" do
+    describe "white pawn" do
+      context "on home row" do
+        let(:moves) { [[0,2], [0,3], [1,2]] }
+        
+        it "returns attack moves and both upward moves (single and double)" do
+          expect(white_pawn.possible_moves).to match_array moves
+        end
+      end
+
+      context "not on home row" do
+        let(:white_pawn_moved) { Pawn.new(:w, [0,2]) }
+        let(:moves) { [[0,3], [1,3]] }
+        
+        it "returns attack moves and one upward move (single)" do
+          expect(white_pawn_moved.possible_moves).to match_array moves
+        end
+      end
+    end
+
+    describe "black pawn" do
+      context "on home row" do
+        let(:moves) { [[0,5], [0,4], [1,5]] }
+        
+        it "returns attack moves and both downward moves (single and double)" do
+          expect(black_pawn.possible_moves).to match_array moves
+        end
+      end
+
+      context "not on home row" do
+        let(:black_pawn_moved) { Pawn.new(:b, [1,4]) }
+        let(:moves) { [[1,3], [0,3], [2,3]] }
+
+        it "returns attack moves and one downward move (single)" do
+          expect(black_pawn_moved.possible_moves).to match_array moves
+        end
+      end
+    end
   end
 end
