f6da91f2f8fe5e30613da6156fb19b1728a85505
Chris Norwood <cnorwood92@gmail.com>
2016-07-27 17:52:09 -0700

    Create valid_moves? method depends on make_valid_moves pruning possibilities.  I should have split this into many more commits...

--- lib/move.rb
+++ lib/move.rb
@@ -1,13 +1,70 @@
 class Move
-  attr_accessor :status
+  attr_accessor :status, :failure, :valid_moves, :temp_moves
   attr_reader   :board, :player, :root, :target
 
   def initialize board, player, root, target
-    @board  = board
-    @player = player
-    @root   = root
-    @target = target
+    @board       = board
+    @player      = player
+    @root        = root
+    @target      = target
+
+    @temp_moves  = []
+    @valid_moves = make_valid_moves
   end
+
+  public
+
+  def valid?
+    valid_moves.include?(target)
+  end
+
+  def make_valid_moves
+    if root_is_players? && (in_possibilities? || in_possible_attacks?)
+      if attack_attempt?
+        temp_moves = possible_attack_moves
+      else
+        temp_moves = possible_moves
+      end
+
+      # prunes positions that contain own piece from valid_moves, but doesn't remove blank squares
+      temp_moves.reject! do |possible| 
+        piece_at_position(possible).color == player_color unless piece_at_position(possible) == false
+      end
+
+      # prunes positions that are not on clear linear path for sliding/non-jumper pieces --> CLEAR `IN_BETWEEN`
+      unless root_is_jumper?
+        temp_moves.reject! { |possible| in_between?(root, possible) }
+      end
+    end
+
+    temp_moves.nil? ? [] : temp_moves
+  end
+
+  def attack_attempt?
+    (target_piece && !target_is_players?) ? true : false
+  end
+
+  def root_is_players?
+    root_color == player_color
+  end
+
+  def target_is_players?
+    target_color == player_color
+  end
+
+  def in_possibilities?
+    possible_moves.find { |move| move == target } ? true : false
+  end
+
+  def in_possible_attacks?
+    possible_attack_moves.find { |move| move == target } ? true : false
+  end
+
+  def root_is_jumper?
+    root_piece.jumper
+  end
+
+  # define important properties of each move:
 
   def root_piece
     piece_at_position(root)
@@ -23,18 +80,6 @@ class Move
 
   def target_color
     target_piece.color if target_piece
-  end
-
-  def root_is_players?
-    root_color == player_color
-  end
-
-  def target_is_players?
-    target_color == player_color
-  end
-
-  def in_possibilities?
-    possible_moves.find { |move| move == target } ? true : false
   end
 
   private
@@ -53,5 +98,94 @@ class Move
 
   def possible_moves
     root_piece.possible_moves
+  end
+
+  def possible_attack_moves
+    root_piece.possible_moves(attacks: true)
+  end
+
+  # helpers for in_between calculations
+  def min(a,b)
+    a < b ? a : b
+  end
+
+  def max(a,b)
+    a > b ? a : b
+  end
+  #######
+
+  def in_between?(from, to)
+    from_x, from_y, to_x, to_y = from[0], from[1], to[0], to[1]
+    
+    rise,    run     = from_y - to_y,     from_x - to_x
+    max_x,   min_x   = max(from_x, to_x), min(from_x, to_x)
+    max_y,   min_y   = max(from_y, to_y), min(from_y, to_y)
+    delta_x, delta_y = max_x - min_x,     max_y - min_y
+
+    if (rise == 0 && run != 0)
+      return in_between_horizontally?(from_y, min_x, max_x, delta_x)
+    end
+
+    if (run == 0 && rise != 0)
+      return in_between_vertically?(from_x, min_y, max_y, delta_y)
+    end
+
+    if (run != 0 && (rise/run == 1 || rise/run == -1))
+      return in_between_diagonally?(from_x, from_y, to_x, to_y, delta_x, delta_y)
+    end
+  end
+
+  def in_between_horizontally?(from_y, min_x, max_x, delta_x)
+    if delta_x > 1
+      status = false
+      (min_x+1).upto(max_x-1) do |try_x|
+        status = true if piece_at_position([try_x, from_y])
+      end
+      return status
+    else
+      false
+    end
+  end
+
+  def in_between_vertically?(from_x, min_y, max_y, delta_y)
+    if delta_y > 1
+      status = false
+      (min_y+1).upto(max_y-1) do |try_y|
+        status = true if piece_at_position([from_x, try_y])
+      end
+      return status
+    else
+      false
+    end
+  end
+
+  def in_between_diagonally?(from_x, from_y, to_x, to_y, delta_x, delta_y)
+    # must be further than one square diagonally away
+    if delta_x > 1 && delta_y > 1
+      status = false
+      # 'to' is left
+      if to_x < from_x
+        (to_x+1).upto(from_x-1) do |try_x|
+          shift_y = try_x-to_x
+          try_y   = (to_y > from_y) ? to_y-shift_y : to_y+shift_y
+
+          status = true if piece_at_position([try_x, try_y])
+        end
+      end
+
+      # 'to' is right
+      if to_x > from_x
+        (from_x+1).upto(to_x-1) do |try_x|
+          shift_y = try_x-from_x
+          try_y   = (to_y > from_y) ? from_y+shift_y : from_y-shift_y
+
+          status = true if piece_at_position([try_x, try_y])
+        end
+      end
+
+      return status
+    else
+      false
+    end
   end
 end
--- spec/move_spec.rb
+++ spec/move_spec.rb
@@ -7,6 +7,9 @@ require 'chess'
 describe Move do
   let(:board) { Board.new }
   let(:white_player) { Player.new(:w) }
+  let(:black_player) { Player.new(:b) }
+
+  let(:played_board) { Board.new }
 
   describe "#root_piece" do
     context "there is piece at root" do
@@ -100,6 +103,35 @@ describe Move do
     end
   end
 
+  describe "#in_possible_attacks?" do
+    context "when Pawn requests an attack diagonally" do
+      # this is pawn moving diagonally
+      let(:move) { Move.new(board, white_player, [0,1], [1,2])}
+
+      it "returns true" do
+        expect(move.in_possible_attacks?).to eql true
+      end
+    end
+
+    context "when Knight requests attack in normal move possiblities" do
+      # knight moving normally from home position
+      let(:move) { Move.new(board, white_player, [1,0], [2,2]) } 
+
+      it "returns true" do
+        expect(move.in_possible_attacks?).to eql true
+      end
+    end
+
+    context "when Pawn requests an attack forward" do
+      # this is pawn moving forward
+      let(:move) { Move.new(board, white_player, [0,1], [0,2])}
+      
+      it "returns false" do
+        expect(move.in_possible_attacks?).to eql false
+      end
+    end
+  end
+
   describe "#target_is_players?" do
     context "target belongs to player" do
       let(:move) { Move.new(board, white_player, [1,0], [3,1]) } 
@@ -115,6 +147,149 @@ describe Move do
       it "returns false" do
         expect(move.target_is_players?).to eql false
       end 
+    end
+  end
+
+  describe "#make_valid_moves" do
+    context "when Knight considered from home row (with valid target)" do
+      let(:move) { Move.new(board, white_player, [1,0], [2,2]) }
+      let(:exp_valid_moves) { [[2,2], [0,2]] }
+
+      it "returns two valid moves (from three possible)" do
+        expect(move.make_valid_moves).to match_array exp_valid_moves
+      end
+    end
+
+    context "when Pawn considered from home row (with valid target)" do
+      let(:move) { Move.new(board, white_player, [2,1], [2,2]) }
+      let(:exp_valid_moves) { [[2,2], [2,3]] }
+
+      it "returns two valid moves" do
+        expect(move.make_valid_moves).to match_array exp_valid_moves
+      end
+    end
+
+    context "when Queen considered from home position" do
+      context "with valid target" do
+        let(:move) { Move.new(board, white_player, [3,0], [4,1]) }
+
+        it "returns no valid moves" do
+          expect(move.make_valid_moves).to match_array []
+        end
+      end
+
+      context "with invalid target" do
+        let(:move) { Move.new(board, white_player, [3,0], [5,5]) }
+
+        it "returns no valid moves" do
+          expect(move.make_valid_moves).to match_array []
+        end
+      end
+    end
+
+    context "when black Bishop considered from home position" do
+      context "with valid target" do
+        let(:move) { Move.new(board, black_player, [2,7], [0,5]) }
+
+        it "returns no valid moves" do
+          expect(move.make_valid_moves).to match_array []
+        end
+      end
+
+      context "with invalid target" do
+        let(:move) { Move.new(board, black_player, [2,7], [0,0]) }
+
+        it "returns no valid moves" do
+          expect(move.make_valid_moves).to match_array []
+        end
+      end
+    end
+
+    context "Board is NOT in default, init. layout" do
+      before do
+        played_board.pieces = []
+        played_board.pieces << King.new(:w, [1,0]) << Queen.new(:w, [4,2])
+        played_board.pieces << Pawn.new(:b, [4,6]) << Pawn.new(:b, [6,6]) << King.new(:b, [6,7]) << Bishop.new(:b, [6,4])
+      end
+
+      describe "Black Bishop" do
+        context "when target is outside of possibilities" do
+          let(:move) { Move.new(played_board, black_player, [6,4], [3,4]) }
+          
+          it "returns no valid moves" do
+            expect(move.make_valid_moves).to match_array []
+          end
+        end
+
+        context "when invalid target within possibilities" do
+          let(:move) { Move.new(played_board, black_player, [6,4], [3,1]) }
+          let(:expected_moves) { [[7,5], [5,3], [4,2], [5,5], [7,3]] }
+          
+          it "returns the piece's, actual valid moves" do
+            expect(move.make_valid_moves).to match_array expected_moves
+          end
+        end
+      end
+    end
+  end
+
+  describe "#valid?" do
+    before do
+      played_board.pieces = []
+      played_board.pieces << King.new(:w, [1,0]) << Queen.new(:w, [4,2]) << Pawn.new(:w, [3,5])
+      played_board.pieces << Pawn.new(:b, [4,6]) << Pawn.new(:b, [6,6]) << King.new(:b, [6,7]) << Bishop.new(:b, [6,4])
+    end
+
+    context "Player requests to move opponent's piece" do
+      let(:move) { Move.new(played_board, white_player, [6,6], [5,3]) }
+      
+      it "returns false" do
+        expect(move.valid?).to eql false
+      end
+    end
+
+    context "Player requests to attack their own piece" do
+      let(:move) { Move.new(played_board, black_player, [6,7], [6,6]) }
+      
+      it "returns false" do
+        expect(move.valid?).to eql false
+      end
+    end
+
+    context "Player requests move outside of that piece's ability" do
+      context "Pawn requests to attack into empty square" do
+        let(:move) { Move.new(played_board, black_player, [4,6], [5,5]) }
+        
+        it "returns false" do
+          expect(move.valid?).to eql false
+        end
+      end
+
+      context "Pawn requests to move two spaces while not on home row" do
+        let(:move) { Move.new(played_board, white_player, [3,5], [3,7]) }
+        
+        it "returns false" do
+          expect(move.valid?).to eql false
+        end
+      end
+    end
+
+    context "Queen requests a valid move" do
+      context "when attacking" do
+        let(:move) { Move.new(played_board, white_player, [4,2], [4,6]) }
+
+        it "returns true" do
+          expect(move.valid?).to eql true
+        end
+      end
+
+      context "when not attacking" do
+        let(:move) { Move.new(played_board, white_player, [4,2], [0,6]) }
+
+        it "returns true" do
+          expect(move.valid?).to eql true
+        end
+      end
     end
   end
 end
